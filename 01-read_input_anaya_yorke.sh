95223baf38bc2526cf574db063e4b1cba7d27418
anaya yorke <ayorke@ggc.edu>
2026-02-01 22:39:06 -0500

    Add my bash script

---
+++ 01-read_input_anaya_yorke.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello from anaya yorke! What is your name"
+read name
+echo "Welcome, $name"
+echo "How old are you?"
+read age
+echo 'Thanks anaya! I see you are ' $age ' years old'
+
+echo "What school do you currently attend?"
+read school
+echo 'Nice! ' $school ' sounds like a great place to learn.'
